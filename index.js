883b5f73a0e8eb2857affd22a43d4c22efa07091
akash kesharwani <akashkesharwani81@gmail.com>
2022-08-12 08:37:49 +0530

    add

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const app = express();
 app.get('/',(req,res)=>{
     res.send('hello world');
 })
+app.get('/about',(req,res)=>{
+    res.send("my name is akash kesharwani");
+})
 app.listen(3000,()=>{
     console.log("connected");
 })
